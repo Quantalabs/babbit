0e731fa02ae40616f5c2f6a5d5868700475ddb9b
QLabs <55121845+Quantalabs@users.noreply.github.com>
2024-05-12 13:05:32 -0700

    feat: :sparkles: add interval struct
    Work with intervals and interval classes.

---
+++ src/interval.rs
@@ -0,0 +1,81 @@
+/// An interval between two notes.
+#[derive(Debug, Clone, Copy)]
+pub struct Interval {
+    /// The number of semitones between the two notes.
+    pub interval: u8,
+}
+
+impl Interval {
+    /// Create an interval from two notes.
+    ///
+    /// Finds the interval between the two notes where `a` is the lower note and `b` is the higher note, and `a` and `b` are both within an octave.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use babbit::interval::Interval;
+    ///
+    /// let some_interval = Interval::from(0, 2); // 2 semitones
+    /// let another_interval = Interval::from(3, 2); // 11 semitones
+    /// ```
+    pub fn from(a: i8, b: i8) -> Interval {
+        Interval {
+            interval: (b - a).rem_euclid(12) as u8,
+        }
+    }
+
+    /// Get the interval class.
+    ///
+    /// Returns the [interval class] of the interval.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use babbit::interval::Interval;
+    ///
+    /// let some_interval = Interval::from(3, 2); // 11 semitones
+    /// let interval_class = some_interval.class(); // ic 1
+    /// ```
+    ///
+    /// [interval class]: https://en.wikipedia.org/wiki/Interval_class
+    pub fn class(&self) -> u8 {
+        if self.interval > 6 {
+            6 - (self.interval % 6)
+        } else {
+            self.interval
+        }
+    }
+}
+
+impl PartialEq for Interval {
+    fn eq(&self, other: &Interval) -> bool {
+        self.class() == other.class()
+    }
+}
+
+impl Eq for Interval {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn interval_from() {
+        let some_interval = Interval::from(3, 2);
+
+        assert_eq!(some_interval.interval, 11);
+    }
+
+    #[test]
+    fn interval_class() {
+        let some_interval = Interval::from(3, 2);
+
+        assert_eq!(some_interval.class(), 1);
+    }
+
+    #[test]
+    fn interval_peq() {
+        let some_interval = Interval::from(3, 2);
+        let another_interval = Interval::from(2, 3);
+
+        assert_eq!(some_interval, another_interval);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 //! A programmatic implementation of common techniques used in serialism and atonal composition.
 //! 
 //! Babbit is named after the 20th century composer Milton Babbit. It provides a programmatic way to work with serialism, tone rows, tone matricies, and other techniques relating to the Second Viennese School and atonal composition.
+
+/// Work with intervallic relationships between notes.
+pub mod interval;
