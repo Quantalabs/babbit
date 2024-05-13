d62b1c8ff6355c18c01b4f48c4ebfc9aae8af140
QLabs <55121845+Quantalabs@users.noreply.github.com>
2024-05-12 17:49:36 -0700

    test: :truck: move tests to seperate directory

--- src/interval.rs
+++ src/interval.rs
@@ -52,30 +52,3 @@ impl PartialEq for Interval {
 }
 
 impl Eq for Interval {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn interval_from() {
-        let some_interval = Interval::from(3, 2);
-
-        assert_eq!(some_interval.interval, 11);
-    }
-
-    #[test]
-    fn interval_class() {
-        let some_interval = Interval::from(3, 2);
-
-        assert_eq!(some_interval.class(), 1);
-    }
-
-    #[test]
-    fn interval_peq() {
-        let some_interval = Interval::from(3, 2);
-        let another_interval = Interval::from(2, 3);
-
-        assert_eq!(some_interval, another_interval);
-    }
-}
